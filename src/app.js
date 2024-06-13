2fbfad90b8b08090cb29a9fb45fa5e5022a195c0
Win Htet Aung <winhtetaung.real@gmail.com>
2024-06-13 09:05:41 +0630

    With Patterns

--- src/app.js
+++ src/app.js
@@ -5,9 +5,11 @@ const taskTotal = document.querySelector("#task-total");
 const listGroup = document.querySelector("#list-group");
 const doneTaskTotal = document.querySelector("#done-task-total");
 
+//Actions or Business Logic
 //To create new list
 const createNewList = (currentTask) => {
   const list = document.createElement("div");
+  list.id = "List" + Date.now();
   list.classList.add("list");
   list.innerHTML = `<div
     class="flex justify-between border-2 border-stone-900 p-4 mb-3"
@@ -112,18 +114,78 @@ const listDoneTotal = () => {
   doneTaskTotal.innerText = lists.length;
 };
 
-//Process
-const addList = () => {
-  listGroup.append(createNewList(taskInput.value));
+//To delete list
+const deleteList = (listId) => {
+  const currentList = document.querySelector(`#${listId}`);
+  if (window.confirm("Are you sure to delete")) {
+    currentList.remove();
+    updateTaskList();
+    listDoneTotal();
+  }
+};
+
+//To edit list
+const editList = (listId) => {
+  const currentList = document.querySelector(`#${listId}`);
+  const editBtn = currentList.querySelector(".edit-btn");
+  const listPara = currentList.querySelector(".list-para");
+
+  editBtn.setAttribute("disabled", true);
+  const newTaskInput = document.createElement("input");
+  newTaskInput.className =
+    "border border-stone-900 pl-2 focus-visible:outline-none";
+  listPara.after(newTaskInput);
+  listPara.classList.add("hidden");
+  newTaskInput.value = listPara.innerText;
+  newTaskInput.focus();
+
+  newTaskInput.addEventListener("change", () => {
+    editBtn.removeAttribute("disabled");
+    listPara.innerText = newTaskInput.value;
+    listPara.classList.remove("hidden");
+    newTaskInput.remove();
+  });
+};
+
+//To check done list
+const doneList = (listId) => {
+  const currentList = document.querySelector(`#${listId}`);
+  const listDoneCheck = currentList.querySelector(".list-done-check");
+  const editBtn = currentList.querySelector(".edit-btn");
+  const listPara = currentList.querySelector(".list-para");
+
+  if (listDoneCheck.checked) {
+    editBtn.setAttribute("disabled", true);
+  } else {
+    editBtn.removeAttribute("disabled");
+  }
+  listDoneTotal();
+  listPara.classList.toggle("line-through");
+  currentList.classList.add("duration-300");
+  currentList.classList.toggle("scale-75");
+  currentList.classList.toggle("opacity-20");
+};
+
+//Handler Functions
+const addList = (task) => {
+  listGroup.append(createNewList(task));
   taskInput.value = null;
   updateTaskList();
 };
 
-//Event on add task btn
-addTaskBtn.addEventListener("click", () => {
-  //to add new list
-  addList();
-});
+const addTaskBtnHandler = () => {
+  if (taskInput.value.trim() ? true : false) {
+    addList(taskInput.value);
+  }
+};
+
+const taskInputHandler = (event) => {
+  if (event.key === "Enter") {
+    if (taskInput.value.trim() ? true : false) {
+      addList(taskInput.value);
+    }
+  }
+};
 
 const listGroupHandler = (event) => {
   // console.log(event.target);
@@ -132,50 +194,23 @@ const listGroupHandler = (event) => {
 
   const list = event.target.closest(".list");
 
-  //Selectors From The List
-  const listDoneCheck = list.querySelector(".list-done-check");
-  const listPara = list.querySelector(".list-para");
-  const deleteBtn = list.querySelector(".delete-btn");
-  const editBtn = list.querySelector(".edit-btn");
-
   if (event.target.classList.contains("list-done-check")) {
-    if (listDoneCheck.checked) {
-      editBtn.setAttribute("disabled", true);
-    } else {
-      editBtn.removeAttribute("disabled");
-    }
-    listDoneTotal();
-    listPara.classList.toggle("line-through");
-    list.classList.add("duration-300");
-    list.classList.toggle("scale-75");
-    list.classList.toggle("opacity-20");
+    doneList(list.id);
   }
 
   if (event.target.classList.contains("delete-btn")) {
-    if (window.confirm("Are you sure to delete")) {
-      list.remove();
-      updateTaskList();
-      listDoneTotal();
-    }
+    deleteList(list.id);
   }
 
   if (event.target.classList.contains("edit-btn")) {
-    editBtn.setAttribute("disabled", true);
-    const newTaskInput = document.createElement("input");
-    newTaskInput.className =
-      "border border-stone-900 pl-2 focus-visible:outline-none";
-    listPara.after(newTaskInput);
-    listPara.classList.add("hidden");
-    newTaskInput.value = listPara.innerText;
-    newTaskInput.focus();
-
-    newTaskInput.addEventListener("change", () => {
-      editBtn.removeAttribute("disabled");
-      listPara.innerText = newTaskInput.value;
-      listPara.classList.remove("hidden");
-      newTaskInput.remove();
-    });
+    editList(list.id);
   }
 };
+
+//Listener
+//Event on add task btn
+addTaskBtn.addEventListener("click", addTaskBtnHandler);
 //Listen Event On The Parent listGroup
 listGroup.addEventListener("click", listGroupHandler);
+//Listen Event On the taskInput
+taskInput.addEventListener("keyup", taskInputHandler);
